6df698d21e9e6482cf1c9e9b79f7a27fa795b391
Paolo <paolo.quintero@avantica.net>
2017-12-19 14:04:26 -0500

    fn

--- src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
@@ -48,13 +48,13 @@ public class PetTypeFormatterTests {
     public void shouldParse() throws ParseException {
         Mockito.when(this.pets.findPetTypes()).thenReturn(makePetTypes());
         PetType petType = petTypeFormatter.parse("Bird", Locale.ENGLISH);
-        assertEquals("Bird", petType.getName());
+        assertEquals("Bird1", petType.getName());
     }
 
     @Test(expected = ParseException.class)
     public void shouldThrowParseException() throws ParseException {
         Mockito.when(this.pets.findPetTypes()).thenReturn(makePetTypes());
-        petTypeFormatter.parse("Fish1", Locale.ENGLISH);
+        petTypeFormatter.parse("Fish", Locale.ENGLISH);
     }
 
     /**
